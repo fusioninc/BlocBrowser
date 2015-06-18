18ae100646b4b9526c885bc094b4822081e88496
Jason Kuang <jason.jkuang@gmail.com>
2015-06-17 23:59:35 -0700

    Added viewControll frame with webView and URL request

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+
+@interface ViewController () <WKNavigationDelegate>
+
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UITextField *textField;
 
 @end
 
 @implementation ViewController
+
+- (void)loadView {
+    UIView *mainView = [UIView new];
+    self.view = mainView;
+    
+    self.webView = [[WKWebView alloc] init];
+    self.webView.navigationDelegate = self;
+    
+    NSString *urlString = @"http://wikipedia.org";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    
+    [mainView addSubview:self.webView];
+    
+    self.view = mainView;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    //make the webview fill the main view
+    self.webView.frame = self.view.frame;
 }
+
 
 @end
